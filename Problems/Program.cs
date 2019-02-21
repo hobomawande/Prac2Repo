b5a0279d91adade30a9b59fb6524474aaa5ccd9b
Mawande Hobo <G17H5759@ict.ru.ac.za>
2019-02-21 15:31:42 +0200

    Added all project files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 
+
 namespace Problems
 {
-    class Program
+  public  class Program
     {
+        public string Prefix(string input){
+          if(input==null) return "0,0:";
+          int count=input.Length;
+      string show;
+          int howMany=0;
+          string[] splt= input.Split( );
+          foreach(string xs in splt){
+            
+              howMany++;
+          }
+            show =$"{count},{howMany} :{input}";
+        return show;
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
